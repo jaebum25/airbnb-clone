816a5aff828781e7b44dff8b78ce42c395d24552
Jaebum Lee <brian.lee025@gmail.com>
2023-05-24 11:30:33 -0400

    login and logout function

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import { Nunito } from "next/font/google";
+import getCurrentUser from "./actions/getCurrentUser";
+import LoginModal from "./components/modals/LoginModal";
 import RegisterModal from "./components/modals/RegisterModal";
 import Navbar from "./components/navbar/Navbar";
 
@@ -14,17 +16,19 @@ const font = Nunito({
   subsets: ["latin"],
 });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const currentUser = await getCurrentUser();
   return (
     <html lang="en">
       <body className={font.className}>
         <ToasterProvier />
+        <LoginModal />
         <RegisterModal />
-        <Navbar />
+        <Navbar currentUser={currentUser} />
         {children}
       </body>
     </html>
