e5095e4d00422baaa3de319b320c52707ece31c9
Jaebum Lee <brian.lee025@gmail.com>
2023-05-25 01:20:32 -0400

    cloudinary image upload rentModal

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -8,6 +8,7 @@ import Heading from "../Heading";
 import CategoryInput from "../Inputs/CategoryInput";
 import Counter from "../Inputs/Counter";
 import CountrySelect from "../Inputs/CountrySelect";
+import ImageUpload from "../Inputs/ImageUpload";
 import { categories } from "../navbar/Categories";
 import Modal from "./Modal";
 
@@ -53,6 +54,7 @@ const RentModal: React.FC<RentModalProps> = () => {
   const guestCount = watch("guestCount");
   const roomCount = watch("roomCount");
   const bathroomCount = watch("bathroomCount");
+  const imageSrc = watch("imageSrc");
 
   const Map = useMemo(
     () =>
@@ -157,6 +159,21 @@ const RentModal: React.FC<RentModalProps> = () => {
           subtitle="How many bathrooms do you have?"
           value={bathroomCount}
           onChange={(value) => setCustomValue("bathroomCount", value)}
+        />
+      </div>
+    );
+  }
+
+  if (step == STEPS.IMAGES) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Add a photo of your place"
+          subtitle="Show guest what your place looks like"
+        />
+        <ImageUpload
+          value={imageSrc}
+          onChange={(value) => setCustomValue("imageSrc", value)}
         />
       </div>
     );
