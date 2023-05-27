0b41a813f296725b18b8f9abe8ed3745791b3627
Jaebum Lee <brian.lee025@gmail.com>
2023-05-27 18:46:03 -0400

    build fix

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,14 +1,15 @@
 import EmptyState from "@/app/components/EmptyState";
 
 import getCurrentUser from "@/app/actions/getCurrentUser";
-import getFavoriteListings from "../actions/getFavoriteListings";
+import getFavoriteListings from "@/app/actions/getFavoriteListings";
+
 import FavoritesClient from "./FavoritesClient";
 
 const ListingPage = async () => {
   const listings = await getFavoriteListings();
   const currentUser = await getCurrentUser();
 
-  if (listings.length == 0) {
+  if (listings.length === 0) {
     return (
       <EmptyState
         title="No favorites found"
