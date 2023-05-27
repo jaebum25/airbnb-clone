bd42e4609bcd15a16c8185b5b327b1ac7cfff879
Jaebum Lee <brian.lee025@gmail.com>
2023-05-27 19:17:34 -0400

    build fix

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,27 +1,18 @@
 import { Listing, Reservation, User } from "@prisma/client";
 
-export type SafeListing = Omit<
-  Listing,
-  'createdAt'
-> & {
+export type SafeListing = Omit<Listing, 'createdAt'> & {
   createdAt: string;
-}
+};
 
-export type SafeReservation = Omit<
-Reservation,
-'createdAt' | 'startDate' | 'endDate' | 'listing'
-> & {
-  createdAt: string,
-  startDate: string,
-  endDate: string,
-  listing: SafeListing
-}
+export type SafeReservation = Omit<Reservation, 'createdAt' | 'startDate' | 'endDate' | 'listing'> & {
+  createdAt: string;
+  startDate: string;
+  endDate: string;
+  listing: SafeListing;
+};
 
-export type SafeUser = Omit<
-User,
-"createdAt" | "updatedAt" | "emailVerified"
-> & {
+export type SafeUser = Omit<User, "createdAt" | "updatedAt" | "emailVerified"> & {
   createdAt: string;
   updatedAt: string;
-  emailVerified: string | null
-}
+  emailVerified?: string | null;
+};
